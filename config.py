e2649f5232555e4b93abde36aebf0b7403a5b1ef
Priyam <50884372+r-priyam@users.noreply.github.com>
2022-04-26 08:45:50 +0530

    fix: pyright errors

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ from pydantic import BaseSettings
 
 
 class Config(BaseSettings):
-    COC_EMAIL: str
-    COC_PASSWORD: str
-    COC_KEY_NAME: str
+    COC_EMAIL: str = ''
+    COC_PASSWORD: str = ''
+    COC_KEY_NAME: str = ''
 
     class Config:
         env_file = ".env"
